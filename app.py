2933159932662966615ade0b82471b085e821153
dhiraj2301 <dhirajshetty91@yahoo.com>
2021-09-23 22:48:17 +0530

    login page html css

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Function to call Login Page
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
